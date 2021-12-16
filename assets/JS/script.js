0c3b523df9f1c6251f347814414e2506b0c1e99e
sharonwhite1977 <sharonwhite1977@gmail.com>
2021-12-16 11:03:00 -0500

    first commit

---
+++ assets/JS/script.js
@@ -0,0 +1,10 @@
+let firstBox;
+
+allColors - ["red", "green", "blue", "yellow", "purple", "gold", "brown", "orange" ]
+
+
+function changeButton() {
+    firstBox = document.getElementById("first");
+    firstBox.style.background = "blue";
+    firstBox.style.border = "dotted 2px maroon"
+}
